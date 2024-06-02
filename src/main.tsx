788bcd2b89ce1073d86e43d5a0ac638927f6f893
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-06-01 20:52:46 -0500

    Add createBrowserRouter with Layout + ErrorPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import ErrorPage from './pages/error-page';
+
+const router = createBrowserRouter([
+  {
+    id: 'root',
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
