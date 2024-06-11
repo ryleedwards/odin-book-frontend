57b96f503ee5706fe2fb0a7140d8c7942a105d3c
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-06-11 11:20:50 -0500

    Handle null value for profile picture

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -2,7 +2,11 @@ const ProfileImage = ({ url }: { url: string }) => {
   return (
     <div className='rounded-full h-48 w-48'>
       <img
-        src={url}
+        src={
+          url
+            ? url
+            : 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg'
+        }
         alt='profile-image'
         className='rounded-full h-full w-full object-cover'
       />
