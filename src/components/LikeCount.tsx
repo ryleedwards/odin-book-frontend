f4e0bac17c1f5fa503ea842e62720b02de3c087b
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-06-16 21:26:13 -0500

    Cap # of likes shown in tooltip

--- src/components/LikeCount.tsx
+++ src/components/LikeCount.tsx
@@ -13,14 +13,28 @@ type LikeCountProps = {
 };
 
 export const LikeCount = ({ post, className, likeCount }: LikeCountProps) => {
-  const likedByList = (
-    <ul>
-      {post.likes.map((like) => (
-        <li key={like.id}>{like.user.name}</li>
-      ))}
-    </ul>
-  );
-
+  let likedByList = null;
+  if (post.likes.length === 0) {
+    likedByList = 'Be the first to like this post!';
+  }
+  if (post.likes.length > 0 && post.likes.length < 4) {
+    likedByList = (
+      <ul>
+        {post.likes.map((like) => (
+          <li key={like.id}>{like.user.name}</li>
+        ))}
+      </ul>
+    );
+  } else if (post.likes.length >= 4) {
+    likedByList = (
+      <ul>
+        {post.likes.slice(0, 3).map((like) => (
+          <li key={like.id}>{like.user.name}</li>
+        ))}
+        <li className='italic'>and {post.likes.length - 3} others</li>
+      </ul>
+    );
+  }
   return (
     <div className={className}>
       <TooltipProvider>
