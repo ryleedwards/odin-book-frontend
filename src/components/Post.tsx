7f4a0eb3da3b04a77649dbb4884611088b1ce179
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-06-22 14:48:05 -0500

    Adjust expand post link to be relative path

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -102,7 +102,7 @@ const Post = ({ post }: PostProps) => {
                 </p>
               </div>
             </div>
-            <Link to={`/posts/${post.id}`} preventScrollReset={true}>
+            <Link to={`posts/${post.id}`} preventScrollReset={true}>
               <FaExpandAlt className='text-xl text-gray-500 hover:text-gray-800' />
             </Link>
           </div>
