02e4986e9d86284c6dd64edd8010d7e85fddf4f5
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-06-25 21:21:21 -0500

    Add following and all feed buttons

--- src/routes/posts.tsx
+++ src/routes/posts.tsx
@@ -1,8 +1,9 @@
 import Feed from '@/components/Feed';
-import { useLoaderData, Outlet } from 'react-router';
+import { useLoaderData, Outlet, useLocation } from 'react-router';
 import { Post as PostType } from '@/types/Post';
 import { deleteLike, getPosts } from '@/api/post';
 import { createLike } from '@/api/post';
+import { NavLink } from 'react-router-dom';
 
 const loader = async () => {
   const posts = await getPosts();
@@ -24,9 +25,34 @@ const action = async ({ request }: { request: Request }) => {
 
 const Posts = () => {
   const posts = useLoaderData() as PostType[] | null;
+  const feedSelectorStyle = 'w-full h-full py-4 text-center';
+  const { search } = useLocation();
+  const view = new URLSearchParams(search).get('view');
 
   return (
     <>
+      <div className='  md:max-w-2xl flex mb-2 rounded-md text-xl justify-center w-full shadow-sm'>
+        <NavLink
+          to={'/posts?view=following'}
+          className={({ isActive }) =>
+            isActive && view === 'following'
+              ? `${feedSelectorStyle} bg-blue-700 text-white rounded-l-md`
+              : `${feedSelectorStyle} rounded-l-md`
+          }
+        >
+          Following
+        </NavLink>
+        <NavLink
+          to={'/posts?view=all'}
+          className={({ isActive }) =>
+            isActive && view === 'all'
+              ? `${feedSelectorStyle} bg-blue-700 text-white rounded-r-md`
+              : `${feedSelectorStyle} rounded-r-md`
+          }
+        >
+          All
+        </NavLink>
+      </div>
       <Feed className='md:max-w-2xl' posts={posts} isFromProfile={false} />
       <Outlet />
     </>
