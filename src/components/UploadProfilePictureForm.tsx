746f6611131d431d123f74e5f14d25ebf97a5995
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-07-11 16:59:25 -0500

    Add preventDefault to cancel btn

--- src/components/UploadProfilePictureForm.tsx
+++ src/components/UploadProfilePictureForm.tsx
@@ -8,15 +8,9 @@ import {
 } from './ui/card';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
-import { useState } from 'react';
 
 const UploadProfilePictureForm = () => {
   const navigate = useNavigate();
-  const [file, setFile] = useState<File | null>(null);
-  const handleSelectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
-    setFile(e.target.files[0]);
-  };
 
   return (
     <Form
@@ -37,7 +31,13 @@ const UploadProfilePictureForm = () => {
           />
         </CardContent>
         <CardFooter className='flex justify-between'>
-          <Button variant='outline' onClick={() => navigate(-1)}>
+          <Button
+            variant='outline'
+            onClick={(e) => {
+              e.preventDefault();
+              navigate(-1);
+            }}
+          >
             Cancel
           </Button>
           <Button type='submit'>Upload</Button>
